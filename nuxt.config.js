8dad3025649d7399027322ac34b827baab8173b9
Taiki Shiraishi <tiki.t.s0118@gmail.com>
2019-04-12 11:04:15 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,117 @@
+import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
+import axios from 'axios'
+import pkg from './package'
+
+const apiUrl = 'http://necotiblog.wp.xdomain.jp'
+
+export default {
+  mode: 'universal',
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
+  css: ['~/assets/style/app.styl'],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['@/plugins/vuetify'],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
+  ],
+  /*
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    transpile: ['vuetify/lib'],
+    plugins: [new VuetifyLoaderPlugin()],
+    loaders: {
+      stylus: {
+        import: ['~assets/style/variables.styl']
+      }
+    },
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  generate: {
+    interval: 1000,
+    routes() {
+      return Promise.all([
+        axios.get(`${apiUrl}/wp-json/wp/v2/posts?per_page=100&page=1&_embed=1`),
+        axios.get(`${apiUrl}/wp-json/wp/v2/pages?per_page=100&page=1&_embed=1`)
+      ]).then(data => {
+        const posts = data[0]
+        const pages = data[1]
+        return posts.data
+          .map(post => {
+            return {
+              // route: '/post/' + post.slug,
+              route: '/posts/' + post.id,
+              payload: post
+            }
+          })
+          .concat(
+            pages.data.map(page => {
+              return {
+                // route: page.slug,
+                route: '' + page.id,
+                payload: page
+              }
+            })
+          )
+      })
+    }
+  },
+  env: {
+    dev: process.env.NODE_ENV
+  }
+}
---
+++ pages/posts/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        {{ post.title.rendered }}
+      </h1>
+      <span v-html="post.content.rendered"></span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: '_id.vue',
+  data() {
+    return {
+      post: {},
+      error: []
+    }
+  },
+  asyncData({ payload }) {
+    return { post: payload }
+  }
+}
+</script>
+
+<style scoped></style>
