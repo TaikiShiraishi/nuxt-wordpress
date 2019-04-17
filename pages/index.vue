aa836fd5b0a2c7c51a148b457c4f6e34fda61903
Taiki Shiraishi <tiki.t.s0118@gmail.com>
2019-04-17 12:36:36 +0900

    Addd news list

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,73 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
       <v-card>
-        <v-card-title class="headline"
-          >Welcome to the Vuetify + Nuxt.js template</v-card-title
-        >
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank">documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"
-              >discord</a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-              >issue board</a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank"
-            >Nuxt GitHub</a
-          >
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
+        <v-list two-line>
+          <template v-for="(post, index) in posts">
+            <v-list-tile
+              :key="'news' + index"
+              ripple
+              :href="'/posts/' + post.id"
+            >
+              <v-list-tile-content>
+                <v-list-tile-title>{{ post.title.rendered }}</v-list-tile-title>
+                <v-list-tile-sub-title
+                  v-html="post.content.rendered"
+                ></v-list-tile-sub-title>
+              </v-list-tile-content>
+
+              <v-list-tile-action>
+                <v-list-tile-action-text>{{
+                  post.date
+                }}</v-list-tile-action-text>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-divider v-if="index + 1 < posts.length" :key="index"></v-divider>
+          </template>
+        </v-list>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      posts: {}
+    }
   },
-  created() {
-    console.log(process.env.development)
+  asyncData({ $axios }) {
+    const apiUrl = 'http://necotiblog.wp.xdomain.jp'
+    return Promise.all([
+      $axios.$get(`${apiUrl}/wp-json/wp/v2/posts?per_page=100&page=1&_embed=1`),
+      $axios.$get(`${apiUrl}/wp-json/wp/v2/pages?per_page=100&page=1&_embed=1`)
+    ]).then(data => {
+      const posts = data[0]
+        .filter(post => post.categories.indexOf(2) >= 0)
+        .map(post => {
+          const date = new Date(post.date)
+          post.date = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`
+          return post
+        })
+      return { posts }
+    })
   }
+  // created: function() {
+  //   const { $axios } = this
+  //   Promise.all([
+  //     $axios.$get(`${apiUrl}/wp-json/wp/v2/posts?per_page=100&page=1&_embed=1`),
+  //     $axios.$get(`${apiUrl}/wp-json/wp/v2/pages?per_page=100&page=1&_embed=1`)
+  //   ]).then(data => {
+  //     const posts = data[0]
+  //       .filter(post => post.categories.indexOf(2) >= 0)
+  //       .map(post => {
+  //         const date = new Date(post.date)
+  //         post.date = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`
+  //         return post
+  //       })
+  //     this.posts = posts
+  //   })
+  // }
 }
 </script>
